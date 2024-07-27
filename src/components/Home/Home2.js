aa88784165994625251f6e745d7cae52990dc448
NayaKunal30 <kunalnayak3004@gmail.com>
2024-07-27 18:44:23 +0530

    added contact form for feedback

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Form, Button} from "react-bootstrap";
 import myImg from "../../Assets/avatar.png"; // Update the file extension to .png
 import Tilt from "react-parallax-tilt";
 import {
@@ -49,7 +49,54 @@ function Home2() {
             </Tilt>
           </Col>
         </Row>
-        <Row>
+        <Row className="justify-content-center">
+  <Col md={6} className="home-about-contact">
+    <h1 style={{ color: "#FFC107" }}>GET IN TOUCH</h1>
+    <p style={{ color: "#FF69B4" }}>
+      Your Feedback <span style={{ color: "#33CC33" }}>Is Important</span> to me
+    </p>
+    <Tilt
+      className="Tilt"
+      options={{ max: 25, scale: 0.8 }}
+      style={{ width: '100%' }}
+    >
+      <div
+        className="Tilt-inner contact-card"
+        style={{
+          boxShadow: "0 0 10px rgba(0, 128, 128, 0.5)",
+          borderRadius: "10px",
+          padding: "20px",
+          border: "1px solid #0097A7",
+          background: "transparent",
+          width: "80%",
+          margin: "40px auto",
+          textAlign: "left"
+        }}
+      >
+        <Form action="https://api.web3forms.com/submit" method="POST">
+          <input type="hidden" name="access_key" value="877f29ad-1ea2-4783-a435-fbc96799c4ed" />
+          <Form.Group controlId="formName">
+            <Form.Label style={{ color: "#FFC107" }}>Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter your name" name="name" style={{ borderColor: "#0097A7" }} />
+          </Form.Group>
+          <Form.Group controlId="formEmail">
+            <Form.Label style={{ color: "#FFC107" }}>Email</Form.Label>
+            <Form.Control type="email" placeholder="Enter your email" name="email" style={{ borderColor: "#0097A7" }} />
+          </Form.Group>
+          <Form.Group controlId="formMessage">
+            <Form.Label style={{ color: "#FFC107" }}>Message</Form.Label>
+            <Form.Control as="textarea" rows={5} placeholder="Enter your message" name="message" style={{ borderColor: "#0097A7" }} />
+          </Form.Group>
+          <Button variant="primary" type="submit" className="mt-3">
+            Send Message
+          </Button>
+        </Form>
+      </div>
+    </Tilt>
+  </Col>
+</Row>
+
+<Row>
           <Col md={12} className="home-about-social">
             <h1>FIND ME ON</h1>
             <p>
@@ -58,7 +105,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://github.com/NayaKunal30"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -68,7 +115,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/Soumyajit4419"
+                  href=""
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -78,7 +125,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  href=""
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -88,7 +135,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href=""
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
